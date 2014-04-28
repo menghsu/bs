f7090f4f62384267948c57a7a63a530f51f5995f
xumeng <xumeng@xumeng-virtual-machine.(none)>
2014-04-28 17:16:53 +0800

    rename transform to procfs

--- code/procfs/client.cpp
+++ code/procfs/client.cpp
@@ -7,13 +7,15 @@ int main(){
 	ofstream out("/proc/transform");
 	
 	string str;
+	cout<<"user input :";
 	while(cin>>str){
 		if(str=="quit")break;
 		out<<str<<endl;
 		ifstream in("/proc/transform");
 		in>>str;
 		in.close();
-		cout<<str<<endl;
+		cout<<"kern output:"<<str<<endl;
+		cout<<"user input :";
 	}
 	
 	out.close();
