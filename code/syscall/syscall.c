c92b6897dd1c57617da7afffe16fe18784347a4c
Tom <xumeng0321@gmail.com>
2014-04-29 21:48:53 +0800

    实现自定义系统调用 syscall

--- code/syscall/syscall.c
+++ code/syscall/syscall.c
@@ -1,24 +1,11 @@
-#include<linux/kernel.h>
-//#include<linux/module.h>
-#include<linux/modversions.h>
-#include<linux/sched.h>
-#include<asm/uaccess.h>
-#define_NR_testsyscall 191
-extern viod *sys_call+table[];
-asmlinkage int testsyscall()
-{
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-console_print("hello world/n");
-return 0;
+int main(){
+	char *str = "message from user-space!";
+        printf("%ld\n", syscall(349,20,str));
+        return 0;
 }
-int init_module()
-{
 
-sys_call_table[_NR_tsetsyscall]=testsyscall;
-printk("system call testsyscall() loaded success/n");
-return 0;
-}
-void cleanup_module()
-{
-printk("system call testsyscall() unloaded success/n"); 
-}
